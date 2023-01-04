aec853542ef7777fcedb9eed02e72f8b603a1a62
Omar Correa <omarcorrea378@yahoo.com>
2023-01-04 16:54:31 -0500

    added more variables

--- notes.js
+++
@@ -1,7 +0,0 @@
-const validator = require('validator');
-
-const appendFile = require('./server.js');
-appendFile();
-
-console.log(validator.isEmail('omarcorrea378@yahoo.com'))
-console.log(validator.isEmail('omarcorrea378@yfoo'))
--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-const fs = require('fs');
-const appendFile = function () {
-  fs.writeFileSync('notepad.txt', 'i am omar correa');
+const express = require('express');
+const path = require('path');
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+const uniqid = require('uniqid');
+const notes = require('./Develop/db/db.json')
+const app = express();
+const PORT = process.env.PORT || 3001
 
-}
+app.use(express.urlencoded({ extended: true }));
+// adds a middleware for serving static files to Express app
+app.use(express.static('public'));
+app.use(express.json());
 
+let parsedNotes = notes
+// get route for notes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
 
-module.exports = appendFile;
+app.get('/api', (req, res) =>
+  res.json(notes));
 
-
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
